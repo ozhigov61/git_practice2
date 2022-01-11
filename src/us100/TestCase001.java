859e7a8a694882b28514817023885c2fbf241af2
ozhig <ozhigoff@yahoo.com>
2022-01-11 12:13:22 -0700

    initial commit

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -3,6 +3,6 @@ package us100;
 public class TestCase001 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello"); //commit
     }
 }
--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -1,5 +1,9 @@
 package us100;
 
 public class TestCase002 {
+    public static void main(String[] args) {
 
+        //ssss
+        ///step 1 is done
+    }
 }
